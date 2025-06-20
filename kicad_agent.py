3c65396596f89df2055c0dbabddabb8c010643e0
behailuA <txtme.bn@gmail.com>
2025-06-20 15:55:37 +0300

    final commit

--- kicad_agent.py
+++ kicad_agent.py
@@ -12,42 +12,63 @@ client = openai.OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 app = Flask(__name__)
 CORS(app)
 
+def find_all(symbol, s_expr):
+    """Recursively find all lists starting with the given symbol."""
+    found = []
+    if isinstance(s_expr, list):
+        if s_expr and isinstance(s_expr[0], sexpdata.Symbol) and s_expr[0].value() == symbol:
+            found.append(s_expr)
+        for item in s_expr:
+            found.extend(find_all(symbol, item))
+    return found
+
 def parse_kicad_schematic(s_expr):
     """Parse a KiCad schematic S-expression and extract title, components, and nets."""
+    # Find title
     title = None
+    title_blocks = find_all('title_block', s_expr)
+    if title_blocks:
+        for item in title_blocks[0]:
+            if isinstance(item, list) and item and item[0] == sexpdata.Symbol('title'):
+                title = item[1]
+                break
+
+    # Find all components and nets recursively
     components = []
     nets = []
-    for item in s_expr:
-        if isinstance(item, list):
-            if item and item[0] == sexpdata.Symbol('title'):
-                title = item[1] if len(item) > 1 else None
-            elif item and item[0] == sexpdata.Symbol('comp'):
-                ref = value = footprint = None
-                for sub in item:
-                    if isinstance(sub, list):
-                        if sub and sub[0] == sexpdata.Symbol('ref'):
-                            ref = sub[1]
-                        elif sub and sub[0] == sexpdata.Symbol('value'):
-                            value = sub[1]
-                        elif sub and sub[0] == sexpdata.Symbol('footprint'):
-                            footprint = sub[1]
-                components.append({
-                    "ref": ref,
-                    "value": value,
-                    "footprint": footprint
-                })
-            elif item and item[0] == sexpdata.Symbol('net'):
-                code = name = None
-                for sub in item:
-                    if isinstance(sub, list):
-                        if sub and sub[0] == sexpdata.Symbol('code'):
-                            code = sub[1]
-                        elif sub and sub[0] == sexpdata.Symbol('name'):
-                            name = sub[1]
-                nets.append({
-                    "code": code,
-                    "name": name
-                })
+
+    # For components, look for 'symbol' or 'comp'
+    for comp in find_all('symbol', s_expr) + find_all('comp', s_expr):
+        ref = value = footprint = None
+        for sub in comp:
+            if isinstance(sub, list):
+                if sub and sub[0] == sexpdata.Symbol('property'):
+                    if len(sub) > 2 and sub[1] == "Reference":
+                        ref = sub[2]
+                    elif len(sub) > 2 and sub[1] == "Value":
+                        value = sub[2]
+                    elif len(sub) > 2 and sub[1] == "Footprint":
+                        footprint = sub[2]
+        components.append({
+            "ref": ref,
+            "value": value,
+            "footprint": footprint
+        })
+
+    # For nets, look for 'net'
+    for net in find_all('net', s_expr):
+        code = name = None
+        for sub in net:
+            if isinstance(sub, list):
+                if sub and sub[0] == sexpdata.Symbol('code'):
+                    code = sub[1]
+                elif sub and sub[0] == sexpdata.Symbol('name'):
+                    name = sub[1]
+        nets.append({
+            "code": code,
+            "name": name
+        })
+
     return {
         "title": title,
         "components": components,
